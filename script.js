4b6bc0179b8fd8c66de24c0b0c19e1c094209d05
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-14 00:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+ let prices = document.querySelectorAll('[data-ns-test="prices"]');
+    let total = 0;
+    for (let price of prices) {
+        total += parseFloat(price.textContent);
+    }
+
+    let table = document.getElementById('groceryList');
+    let newRow = table.insertRow();
+    let newCell = newRow.insertCell();
+    newCell.setAttribute('colspan', '2');
+    newCell.setAttribute('data-ns-test', 'grandTotal');
+    newCell.textContent = `Total: $${total}`;
